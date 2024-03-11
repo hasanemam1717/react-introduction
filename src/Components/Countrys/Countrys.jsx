8e1cf2c6bca466e61bd9bea57972f32e3128fa74
hasanemam1717 <hasanemam1717@gmail.com>
2024-03-11 12:53:25 +0600

    added background clr

--- src/Components/Countrys/Countrys.jsx
+++ src/Components/Countrys/Countrys.jsx
@@ -6,19 +6,41 @@ import './Countrys.css'
 
 const Countrys = () => {
     const [contrys,setCountrys] = useState([]);
+    const [visitedContrys,setVisitedContrys] = useState([]);
+
     useEffect(() =>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then (data => setCountrys(data))
     },[])
+
+    const handleVisitedCountries = (countries) => {
+        console.log(countries)
+        console.log('visited countries')
+        const newVisitedCountries = [...visitedContrys ,countries]
+        setVisitedContrys(newVisitedCountries)
+    }
+
     console.log(contrys)
     return (
         <div >
             <h2>Countrys : {contrys.length}
+            <div>
+                <h1>Visited Countrys : {visitedContrys.length}</h1>
+                <ul>
+                    {
+                       visitedContrys.map((country) => <li key={country.cca3}>{country.name.common}</li> )
+                    }
+                </ul>
+            </div>
             </h2>
             <div className="contray-container">
             {
-                contrys.map(contry => <Country key={contry.cca3} country={contry}></Country>)
+                contrys.map(contry => <Country 
+                    key={contry.cca3}
+                    handleVisitedCountries={handleVisitedCountries} 
+                    country={contry}
+                    ></Country>)
             }
             </div>
             
--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountries}) => {
     console.log(country)
     const {name,flags,area,population,cca3} = country;
     // console.log(name,flags)
@@ -9,14 +9,18 @@ const Country = ({country}) => {
     const handleVIsited = () => {
         setVisited(!Visited)
     }
-
+    const passPerams = () => {
+        handleVisitedCountries(country)
+    }
+    console.log(handleVisitedCountries)
     return (
-        <div className='country'>
-            <h1>Name: {name.common}</h1>
+        <div className={`country ${Visited ? 'visited':'nonVisited'}`}>
+            <h1 style={{color:Visited?'purple':'white'}}>Name: {name.common}</h1>
             <img src={flags.png} alt="" />
             <h1>Popolation : {population}</h1>
             <h1>Area : {area}</h1>
             <h1>Code : {cca3}</h1>
+            <button onClick={passPerams}>Mark visited</button> &nbsp;&nbsp;
             <button onClick={handleVIsited}>{Visited ? 'visited':'Going'}</button>
             {Visited ? <h1>I have visited this country.</h1>: 'I want to visit this country'}
         </div>
